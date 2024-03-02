88ce683b5fbb3f4ee53afa816399a17863be30e5
Hubert <142225610+hubertrokosz@users.noreply.github.com>
2024-03-02 16:29:44 +0100

    Made working game with DOM display

--- script.js
+++ script.js
@@ -1,33 +1,109 @@
 let game = {
 
     board: [
-        [".", ".", "."],
-        [".", ".", "."],
-        [".", ".", "."]
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "]
     ],
 
-    displayBoard: function() {
-        console.table(this.board.map(row => row.join("|")).join("\n"));
+    win: "",
+
+    currentTurn: "o",
+
+    createPlayer: function(name, xo) {
+
+        return {
+            name: name,
+            xo: xo
+        }
+
     },
 
-    play: function(row, column) {
-        if (this.player.turn == "x") {
+    submit: function() {
 
-            this.board[row][column] = "x";
-  
-            this.player.turn = "o";
-        }
-        else if (this.player.turn == "o") {
+        let form = document.getElementById("form");
 
-            this.board[row][column] = "o";
+        form.addEventListener("submit", function(event) {
 
-            this.player.turn = "x";
-        }
-        this.displayBoard();
-        this.checkWin();
+            event.preventDefault();
+
+            game.makePlayers();
+
+            game.play();
+
+            document.getElementById("player1").value = "";
+            document.getElementById("player2").value = "";
+
+        });
+    },
+
+    restartGame: function() {
+        this.win = "";
+        this.board = [
+            [" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]
+        ]
+        this.displayDOM();
+        this.play();
+    },
+
+    makePlayers: function() {
+        this.player1 = this.createPlayer(document.getElementById("player1").value, "x");
+        this.player2 = this.createPlayer(document.getElementById("player2").value, "o");
+    },
+
+    displayHeader: function(name, turn) {
+        let header = document.getElementById("header");
+
+        header.textContent = name + " turn (" + turn + ")";
+    },
+
+    play: function() {
+
+        if (this.currentTurn == "o") this.displayHeader(this.player1.name, this.player1.xo);
+        else if (this.currentTurn == "x") this.displayHeader(this.player2.name, this.player2.xo);
+
+        let tiles = document.querySelectorAll(".tile");
+
+        tiles.forEach(item => {
+
+            item.addEventListener("click", () => {
+
+                const rowIndex = item.getAttribute("data-row");
+                const colIndex = item.getAttribute("data-col");
+
+                if (!(this.win == "x" || this.win == "o" || this.win == "draw")) {
+                    if (this.board[rowIndex][colIndex] != "x" && this.board[rowIndex][colIndex] != "o") {
+
+                        if (this.currentTurn == "x") {
+                            this.displayHeader(this.player1.name, this.player1.xo);
+                            this.currentTurn = "o";
+                        }
+    
+                        else if (this.currentTurn == "o") {
+                            this.displayHeader(this.player2.name, this.player2.xo);
+                            this.currentTurn = "x";
+                        }
+    
+                        this.board[rowIndex][colIndex] = this.currentTurn;
+                    }
+                }
+
+                this.displayDOM();
+                this.checkWin();
+
+            });
+        });
+
+        let restart = document.getElementById("restart");
+        restart.addEventListener("click", () => {
+            this.restartGame();
+        });
     },
 
     checkWin: function() {
+
         if (this.board[0][0] == "x" && this.board[0][1] == "x" && this.board[0][2] == "x" ||
             this.board[1][0] == "x" && this.board[1][1] == "x" && this.board[1][2] == "x" ||
             this.board[2][0] == "x" && this.board[2][1] == "x" && this.board[2][2] == "x" ||
@@ -36,29 +112,42 @@ let game = {
             this.board[0][2] == "x" && this.board[1][2] == "x" && this.board[2][2] == "x" ||
             this.board[0][0] == "x" && this.board[1][1] == "x" && this.board[2][2] == "x" ||
             this.board[2][0] == "x" && this.board[1][1] == "x" && this.board[0][2] == "x") {
-            console.log("x won!");
+
+            header.textContent = "X won!"; 
+            this.win = "x";
         }
         
         else if (this.board[0][0] == "o" && this.board[0][1] == "o" && this.board[0][2] == "o" ||
-        this.board[1][0] == "o" && this.board[1][1] == "o" && this.board[1][2] == "o" ||
-        this.board[2][0] == "o" && this.board[2][1] == "o" && this.board[2][2] == "o" ||
-        this.board[0][0] == "o" && this.board[1][0] == "o" && this.board[2][0] == "o" ||
-        this.board[0][1] == "o" && this.board[1][1] == "o" && this.board[2][1] == "o" ||
-        this.board[0][2] == "o" && this.board[1][2] == "o" && this.board[2][2] == "o" ||
-        this.board[0][0] == "o" && this.board[1][1] == "o" && this.board[2][2] == "o" ||
-        this.board[2][0] == "o" && this.board[1][1] == "o" && this.board[0][2] == "o") {
-        console.log("o won!");
-    }
-        else if (this.board.every(row => row.every(element => element === "x" || element === "o"))) console.log("draw");
+            this.board[1][0] == "o" && this.board[1][1] == "o" && this.board[1][2] == "o" ||
+            this.board[2][0] == "o" && this.board[2][1] == "o" && this.board[2][2] == "o" ||
+            this.board[0][0] == "o" && this.board[1][0] == "o" && this.board[2][0] == "o" ||
+            this.board[0][1] == "o" && this.board[1][1] == "o" && this.board[2][1] == "o" ||
+            this.board[0][2] == "o" && this.board[1][2] == "o" && this.board[2][2] == "o" ||
+            this.board[0][0] == "o" && this.board[1][1] == "o" && this.board[2][2] == "o" ||
+            this.board[2][0] == "o" && this.board[1][1] == "o" && this.board[0][2] == "o") {
 
+            header.textContent = "O won!";
+            this.win = "o";
+
+        }
+
+        else if (this.board.every(row => row.every(element => element === "x" || element === "o"))) {
+            header.textContent = "Draw!";
+            this.win = "draw";
+        }   
     },
 
-    player: {
-        turn: "x"
+    displayDOM: function() {
+        this.board.forEach((row, rowIndex) => {
+            row.forEach((item, colIndex) => {
+                const selector = `div[data-row="${rowIndex}"][data-col="${colIndex}"]`;
+                const div = document.querySelector(selector);
+                if (div) {
+                    div.textContent = item;
+                }
+            });
+        });
     }
-
 };
 
-
-
-game.displayBoard();
+game.submit();
